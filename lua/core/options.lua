2c8f9a6129148849f144b04673427267855020f2
Daniel Tineli jr <danieltineli.td@gmail.com>
2025-04-28 15:44:04 -0300

    jump to definition on a split panel

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -7,8 +7,7 @@ vim.schedule(function()
 end)
 
 vim.opt.breakindent = true
--- aquela linha cinza, nao sei se quero ainda 
-vim.opt.cursorline = false
+
 -- save undo history
 vim.opt.undofile = true
 vim.o.shiftwidth = 4 -- The number of spaces inserted for each indentation (default: 8)
@@ -16,4 +15,4 @@ vim.o.tabstop = 4 -- Insert n spaces for a tab (default: 8)
 vim.o.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
 vim.o.expandtab = true -- Convert tabs to spaces (default: false)
 vim.o.showmode = false
-
+vim.o.colorcolumn = '80'
