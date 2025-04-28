30908ab4cd3de84dd2827d9b6495760df54615a4
Daniel Tineli jr <danieltineli.td@gmail.com>
2025-04-28 18:51:30 -0300

    go imports

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -29,6 +29,7 @@ return {
       formatting.shfmt.with { args = { '-i', '4' } },
       formatting.terraform_fmt,
       formatting.gofmt,
+      formatting.goimports,
       require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
       require 'none-ls.formatting.ruff_format',
     }
